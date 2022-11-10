774efcef3cb63d88d9ee8cacb02553c3e902ac1d
tvasilopoulou <thomaisv97@gmail.com>
2022-11-10 12:15:19 +0200

    Initial commit

---
+++ Assets/Scripts/CreatePin.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Net.WebSockets;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+using TMPro;
+
+    
+
+public class CreatePin : MonoBehaviour
+{
+    public Texture2D ambulance;
+    public Texture2D cam;
+    public Texture2D cod;
+    public Texture2D firecar;
+    public Texture2D limocar;
+    public Texture2D policecar;
+    public Texture2D riskhigh;
+    public Texture2D risklow;
+    public Texture2D riskmoderate;
+    public Texture2D sportcar;
+    public Texture2D teamk9;
+    public Texture2D teamsecurity;
+    public Texture2D truckcar;
+    public Texture2D ugv;
+    public Texture2D uav;
+    
+    private List<OnlineMapsMarker> OnlineMapsMarkerList;
+    private List<string> IDList;
+
+    class Telemetry{
+        public float latitude;
+        public float longitude;
+        public string item;
+        public string speed;
+        public string altitude;
+        public string id;
+        public string message_type;
+    
+    }
+
+
+    void Start()
+    {
+        OnlineMapsMarkerList = new List<OnlineMapsMarker>();
+        IDList = new List<string>();
+
+        Positions.OnReceived += Place;
+        
+    }
+
+    void Place(string jsonMessage)
+    {
+        Telemetry data = JsonUtility.FromJson<Telemetry>(jsonMessage);
+
+        string label = data.item.ToUpper() + data.id + System.Environment.NewLine + "Lat: " + data.latitude + ", Lon: " + data.longitude + System.Environment.NewLine + 
+                        "Speed: " + data.speed + System.Environment.NewLine + "Altitude: " + data.altitude;
+        
+
+        if(!IDList.Contains(data.id)){
+            OnlineMapsMarker marker = OnlineMapsMarkerManager.CreateItem(new Vector2(data.latitude,data.longitude), itemLocated(data.item), label);
+            marker.scale = 0.5f;
+
+            OnlineMapsMarkerList.Add(marker);
+            IDList.Add(data.id);
+        }
+         
+        else{
+            foreach(OnlineMapsMarker marker in OnlineMapsMarkerList){
+                if(marker.label.Contains(data.id)){
+                    marker.position = new Vector2(data.latitude,data.longitude);
+                    marker.label = label;
+                    break;
+                }
+            }
+        }
+
+
+        
+    }
+
+
+
+
+
+    private Texture2D itemLocated(string item){
+        if(item.IndexOf("ambulance", StringComparison.OrdinalIgnoreCase) >= 0) return ambulance;
+        else if(item.IndexOf("camera", StringComparison.OrdinalIgnoreCase) >= 0) return cam;
+        else if(item.IndexOf("cod", StringComparison.OrdinalIgnoreCase) >= 0) return cod;
+        else if(item.IndexOf("firecar", StringComparison.OrdinalIgnoreCase) >= 0) return firecar;
+        else if(item.IndexOf("limocar", StringComparison.OrdinalIgnoreCase) >= 0) return limocar;
+        else if(item.IndexOf("policecar", StringComparison.OrdinalIgnoreCase) >= 0) return policecar;
+        else if(item.IndexOf("riskhigh", StringComparison.OrdinalIgnoreCase) >= 0) return riskhigh;
+        else if(item.IndexOf("risklow", StringComparison.OrdinalIgnoreCase) >= 0) return risklow;
+        else if(item.IndexOf("riskmoderate", StringComparison.OrdinalIgnoreCase) >= 0) return riskmoderate;
+        else if(item.IndexOf("sportcar", StringComparison.OrdinalIgnoreCase) >= 0) return sportcar;
+        else if(item.IndexOf("teamk9", StringComparison.OrdinalIgnoreCase) >= 0) return teamk9;
+        else if(item.IndexOf("teamsecurity", StringComparison.OrdinalIgnoreCase) >= 0) return teamsecurity;
+        else if(item.IndexOf("truckcar", StringComparison.OrdinalIgnoreCase) >= 0) return truckcar;
+        else if(item.IndexOf("ugv", StringComparison.OrdinalIgnoreCase) >= 0) return ugv;
+        else if(item.IndexOf("uav", StringComparison.OrdinalIgnoreCase) >= 0) return uav;
+        else return riskmoderate;
+
+    }
+}
---
+++ Assets/Scripts/Positions.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+
+using UnityEngine;
+using System.Net;
+using UnityEngine.UI;
+
+public class Positions : MonoBehaviour
+{
+
+    public Text text;
+    public delegate void ReceiveAction(string message);
+    public static event ReceiveAction OnReceived;
+    public static event ReceiveAction OnRoutesReceived;
+    private ClientWebSocket webSocket = null;
+    private ConcurrentQueue<string> messageQueue = new ConcurrentQueue<string>();
+
+    [SerializeField]
+    private string url = "ws://195.134.66.232:6999/broker";
+
+    // class PositionDataType{
+    //     public string message_type;
+    //     public double latitude;
+    //     public double longitude;
+    //     public string item;
+    //     public string speed;
+    //     public string distance;
+
+    //     public void print(){
+    //         Debug.Log( message_type + " " + latitude + " " + longitude + " " + item + " " + speed + " " + distance);
+    //     }
+    // }
+
+    void Start()
+    {
+        Task connect_one = Connect(url);
+    }
+
+    void OnDestroy()
+    {
+        if (webSocket != null)
+        {
+            webSocket.Dispose();
+        }
+        Debug.Log("the socket one is closed");
+
+    }
+
+    public async Task Connect(string url_one)
+    {
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+        try
+        {
+            webSocket = new ClientWebSocket();
+            Debug.Log("Connecting TO " + url);
+            await webSocket.ConnectAsync(new Uri(url), CancellationToken.None);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("EXCEPTION IN CONNECTING TO WEBSOCKET SERVER.");
+            Debug.LogError(e);
+        }
+
+        Debug.Log("receiving message.");
+        await Receive();
+        Debug.LogError("THIS SHOULD NOT PRINT.");
+
+    }
+
+
+    private async Task Send()   // this function is to answer something, currently not called
+    {
+        string message = "some ACK text if needed";
+        var encoded = Encoding.UTF8.GetBytes(message);
+        var buffer = new ArraySegment<Byte>(encoded, 0, encoded.Length);
+
+        await webSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+    private async Task Receive(int length = 400000)
+    {
+        ArraySegment<Byte> buffer = new ArraySegment<byte>(new byte[length]);
+
+        while (webSocket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result = null;
+
+            using (var ms = new MemoryStream())
+            {
+                Debug.Log("start");
+
+                do
+                {
+                    result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+
+                } while (!result.EndOfMessage);
+                 
+                Debug.Log(result.Count);
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    
+                    using (var reader = new StreamReader(ms, Encoding.UTF8))
+                    {
+                        string message = reader.ReadToEnd();
+                        if (message.Contains("New Session with id")){
+                            continue;
+                        }
+                        else if(message.Contains("routes")){
+                            Debug.Log("should not print");
+                            OnRoutesReceived(message);
+                            continue;
+                        }
+
+                        text.text = message;
+
+                        // PositionDataType data = JsonUtility.FromJson<PositionDataType>(message);
+
+                        // // this is needed to have the correct decimal digits!!
+                        // data.latitude = Math.Round(data.latitude, 4, MidpointRounding.ToEven);
+                        // data.longitude = Math.Round(data.longitude, 4, MidpointRounding.ToEven);
+
+                        // text.text = data.message_type + " " + data.latitude + " " + data.longitude + " " + data.speed+ " " + data.distance;
+                        // data.print();
+
+                        //must be delete in case of threads 
+                        //await Task.Run(Process);
+                        //the call may happen elseware
+                        if (OnReceived != null) OnReceived(message);
+                    }
+                }else if(result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+
+                }
+            }
+        }
+    }
+}
